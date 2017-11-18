98016888068d946e55613ec908280052827a4d4c
Felipe <felipe.citnnet@gmail.com>
2017-11-18 17:17:59 -0200

    Add files via upload

---
+++ random.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IFS="
+"
+
+NumReg=$(cat entrada.txt | wc -l)
+
+i=0
+
+j=$((RANDOM % $NumReg))
+
+for Reg in $(cat entrada.txt); do
+	while [ ${aSai[$j]} ]; do
+		j=$((RANDOM % $NumReg))
+	done
+	aSai[$j]="$Reg"
+done
+
+ > saida.txt
+
+for ((i=0; i<=$NumReg-1; i++)); do
+	echo ${aSai[i]} >> saida.txt
+done
+
+cat saida.txt
